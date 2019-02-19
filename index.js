3132527ebc937b0caf128e2fb19e7aaec8bb960c
Christoph Herrmann <christoph.herrmann@7-tv.de>
2019-02-19 17:35:14 +0100

    Add multirow inserts

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ module.exports = client =>
       const result = await client.query(sql`SELECT ${sql.identifiers(conditions ? columns : ['*'])} FROM ${sql.identifier(table)} WHERE ${sql.pairs(conditions ? conditions : columns, ' AND ')}`)
       return result.rows
     },
-    insert: async (table, row, serialColumn = 'id') => {
-      const result = await client.query(sql`INSERT INTO ${sql.identifier(table)} (${sql.identifiers(Object.keys(row))}) VALUES (${sql.values(Object.values(row))}) RETURNING ${sql.identifier(serialColumn)}`)
-      return result.rows[0][serialColumn]
+    insert: async (table, rows, serialColumn = 'id') => {
+      if (!Array.isArray(rows)) {
+        rows = [rows]
+      }
+      const result = await client.query(sql`INSERT INTO ${sql.identifier(table)} (${sql.identifiers(rows[0])}) VALUES ${sql.valuesList(rows)} RETURNING ${sql.identifier(serialColumn)}`)
+      if (rows.length === 1) {
+        return result.rows[0][serialColumn]
+      }
+      return result.rows.map(row => row[serialColumn])
     },
     update: async (table, updates, conditions) => {
       const result = await client.query(sql`UPDATE ${sql.identifier(table)} SET ${sql.pairs(updates, ', ')} WHERE ${sql.pairs(conditions, ' AND ')}`)
